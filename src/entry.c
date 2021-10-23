a16858366c7d22c16eea4aee8f84a5150deaaa28
lanylow <lanylow@protonmail.com>
2021-10-23 18:21:36 +0200

    Add project files.

---
+++ src/entry.c
@@ -0,0 +1,70 @@
+#include <minhook.h>
+
+#include "il2cpp.h"
+
+PVOID get_unlocked_skins(PHAT_MANAGER _this, PVOID info) {
+  return _this->all_skins->array;
+}
+
+PVOID get_unlocked_hats(PHAT_MANAGER _this, PVOID info) {
+  return _this->all_hats->array;
+}
+
+PVOID get_unlocked_pets(PHAT_MANAGER _this, PVOID info) {
+  return _this->all_pets->array;
+}
+
+DWORD WINAPI startup_thread(VOID) {
+  if (!il2cpp_init())
+    return EXIT_FAILURE;
+
+  PVOID domain = il2cpp_domain_get();
+  PIL2CPP_ASSEMBLY assembly = il2cpp_domain_assembly_open(domain, "Assembly-CSharp");
+  SIZE_T cls_index = 0;
+  BYTE state = 0;
+
+  MH_Initialize();
+
+  while (cls_index < il2cpp_image_get_class_count(assembly->image)) {
+    if (state == 7)
+      break;
+
+    PVOID cls = il2cpp_image_get_class(assembly->image, cls_index);
+
+    if (cls != NULL) {
+      PVOID iterator = NULL;
+      PMETHOD_INFO method = NULL;
+
+      while ((method = il2cpp_class_get_methods(cls, &iterator)) != 0) {
+        if ((strcmp(method->method_name, "GetUnlockedSkins") == 0) && !((state & 1) == 1)) {
+          MH_CreateHook(method->method_pointer, (PVOID)&get_unlocked_skins, NULL);
+          state |= 1;
+        }
+
+        if ((strcmp(method->method_name, "GetUnlockedHats") == 0) && !((state & 2) == 2)) {
+          MH_CreateHook(method->method_pointer, (PVOID)&get_unlocked_hats, NULL);
+          state |= 2;
+        }
+
+        if ((strcmp(method->method_name, "GetUnlockedPets") == 0) && !((state & 4) == 4)) {
+          MH_CreateHook(method->method_pointer, (PVOID)&get_unlocked_pets, NULL);
+          state |= 4;
+        }
+      }
+    }
+
+    ++cls_index;
+  }
+
+  MH_EnableHook(MH_ALL_HOOKS);
+
+  return EXIT_SUCCESS;
+}
+
+BOOL APIENTRY DllMain(PVOID instance, DWORD reason, PVOID reserved) {
+  if (reason == DLL_PROCESS_ATTACH) {
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)startup_thread, NULL, 0, NULL);
+  }
+
+  return TRUE;
+}
---
+++ src/il2cpp.h
@@ -0,0 +1,51 @@
+#pragma once
+
+typedef struct _METHOD_INFO {
+  PVOID method_pointer;
+  PVOID invoker_method;
+  PCSTR method_name;
+} METHOD_INFO, *PMETHOD_INFO;
+
+typedef struct _IL2CPP_ASSEMBLY {
+  PVOID image;
+} IL2CPP_ASSEMBLY, *PIL2CPP_ASSEMBLY;
+
+typedef struct _LIST {
+  BYTE pad[8];
+  PVOID array;
+} LIST, *PLIST;
+
+typedef struct _HAT_MANAGER {
+  BYTE pad[24];
+  PLIST all_pets;
+  PLIST all_hats;
+  PLIST all_skins;
+} HAT_MANAGER, *PHAT_MANAGER;
+
+#define DECLARE_FUNC(ret, name, args) \
+	typedef ret (*tmp_##name) args; \
+	tmp_##name name;
+
+DECLARE_FUNC(PVOID, il2cpp_domain_get, ())
+DECLARE_FUNC(PIL2CPP_ASSEMBLY, il2cpp_domain_assembly_open, (PVOID domain, PCSTR name))
+DECLARE_FUNC(SIZE_T, il2cpp_image_get_class_count, (PVOID image))
+DECLARE_FUNC(PVOID, il2cpp_image_get_class, (PVOID image, SIZE_T index))
+DECLARE_FUNC(PMETHOD_INFO, il2cpp_class_get_methods, (PVOID cls, PVOID *iterator))
+
+BOOL il2cpp_init(VOID) {
+  HMODULE game_module = GetModuleHandleA("GameAssembly.dll");
+
+	if (!game_module)
+		return FALSE;
+
+	#define INIT_FUNC(name) \
+		name = (tmp_##name)GetProcAddress(game_module, #name);
+
+	INIT_FUNC(il2cpp_domain_get)
+  INIT_FUNC(il2cpp_domain_assembly_open)
+  INIT_FUNC(il2cpp_image_get_class_count)
+  INIT_FUNC(il2cpp_image_get_class)
+  INIT_FUNC(il2cpp_class_get_methods)
+
+	return TRUE;
+}
